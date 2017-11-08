6726e4296a0094728444580952cde9c6dd8e788e
arneespeel <33489119+arneespeel@users.noreply.github.com>
2017-11-08 15:27:22 +0100

    Add files via upload

---
+++ BuildIT/src/buildit/ConstructionSite.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buildit;
+
+/**
+ *
+ * @author Media Markt Kortrijk
+ */
+public class ConstructionSite {
+    private String address;
+    
+}
---
+++ BuildIT/src/buildit/Employee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buildit;
+
+/**
+ *
+ * @author Media Markt Kortrijk
+ */
+public class Employee {
+    private String employeeID;
+    private Fuction function;
+    private String emplEmail;
+    private String emplPhoneNumber;
+            
+    
+}
